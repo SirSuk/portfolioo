08ca29b8d1e70f13a50e2f7152115ea4ca7aba64
Radif El noun <SIRIOSUKUR@GMAIL.COM>
2022-12-15 08:48:18 +0100

    adding links

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+/* declaraciones */
 const btn= document.getElementById("btnColores");
+
+
+/*background changer */
+
 btn.addEventListener('click', () => {
-        document.body.style.background = colorChange()
+    document.body.style.background = colorChange()
 })
 
 function colorChange () {
-    return `hsl(${Math.floor(Math.random()*360)}, 25% ,70%  )`
+    return `hsl(${Math.floor(Math.random()*360)}, 35% ,70%)`
 }
+
 
